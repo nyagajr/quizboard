9ca7c2fa24b30ad09dff73a29ed94d4c9295b58a
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-11 13:17:08 +0300

    assigning marks to each question

--- js/script.js
+++ js/script.js
@@ -2,19 +2,54 @@ $(document).ready(function(){
   $(".btn").click(function(){
 var answer1 = $("input:radio[name=btn1]:checked").val();
 var answer2 = $("input:radio[name=btn2]:checked").val();
-var answer3 = $("input:text[name=btn3]:").val();
+var answer3 = $("input:radio[name=btn3]:checked").val();
+var answer4 = $("input:radio[name=btn4]:checked").val();
+var answer5 = $("input:radio[name=btn5]:checked").val();
 
-if (answer1.toString() === "no" && answer2.toString()==="America" && answer3.toString()==="moringa prep cohort 14")
+var mark1,mark2,mark3,mark4,mark5 = 0;
+
+if (answer1.toString() === "JavaScript")
 {
-  alert ("You are right");
-} else {
-  alert("You are wrong");
+  mark1 =20;
+} else
+{
+ mark1 =0;
+}
+if (answer2.toString() === "object-based")
+{
+  mark2 =20;
+} else
+{
+ mark2 =0;
+}
+if (answer3.toString() === "Declaration statements")
+{
+  mark3 =20;
+} else
+{
+ mark3 =0;
+}
+if (answer4.toString() === "Classes")
+{
+  mark4 =20;
+} else
+{
+ mark4 =0;
+}
+if (answer5.toString() === "Functions are values, and there is no hard distinction between methods and fields")
+{
+  mark5 =20;
+} else
+{
+ mark5 =0;
+}
+add(mark1,mark2,mark3,mark4,mark5);
 
-// if(answer2.toString() === "America")
-// {
-//   alert ("You are right");
-// } else {
-//   alert("You are wrong");
-// }
 });
 });
+
+function add (num1,num2,num3,num4,num5)
+{
+  var result = num1+num2+num3+num4+num5;
+  alert(result);
+};
